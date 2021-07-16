2433e54937aaab3ecd3fc323e707472a23d25fd1
KOTHAGADIAKHILA <85933864+KOTHAGADIAKHILA@users.noreply.github.com>
2021-07-16 15:31:24 +0530

    Add files via upload

---
+++ PPS LAB PROGRAMMES(JNTUH)/expansion_3_terms.c
@@ -0,0 +1,17 @@
+#include<math.h>
+#include<stdio.h>
+int main()
+{
+	double x,i,sum,sign,m,n,d;
+	printf("enter the x value= ");
+	scanf("%lf",&x);
+	sum=1;
+	for(i=1;i<=3;i++)      
+	{
+		sign=pow(-1,i);
+		m=pow(x,i);
+		sum=sum+((sign*m)/(2*i));
+    }  
+printf("sum=%lf",sum);
+return 0;
+}
